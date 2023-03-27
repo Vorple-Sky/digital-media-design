b27f1d774f648b654eea3bedcbe6ea83c294632a
Vorple-Sky <hannahg3211@gmail.com>
2023-03-27 10:40:26 -0700

    changed sketch

--- p5-start/sketch.js
+++ p5-start/sketch.js
@@ -1,3 +1,4 @@
+let mood;
 function setup() {
     // make the div called "mySketch" a p5.js canvas element
     let cnv = createCanvas(windowWidth, windowHeight);
@@ -9,6 +10,15 @@ function setup() {
     //draw a white circle in the middle of the page
     fill(255);
     ellipse(width/2, height/2, 100,100);
+    background(0);
+    //console.log("x: " + mouseX + "," + "y: " + mouseY);
+    if(mouseX < width/2){
+      mood = "dark";
+    } else{
+      mood ="light";
+    }
+    console.log(mood);
+  
 
   }
   
