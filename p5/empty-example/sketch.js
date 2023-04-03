28ff123a38c3fa1beca63b5bc9134380aa037842
Vorple-Sky <hannahg3211@gmail.com>
2023-04-03 09:47:14 -0700

    added to sketch

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,8 +1,16 @@
+let ypos =200;
+let xpos =80;
+let xstep =60;
+
 function setup() {
   // put setup code here
+  createCanvas(400,400);
+  noLoop();
 }
 
 function draw() {
-  // put drawing code her
+  // put drawing code here
+  background(255);
+  fill()
 
   }
