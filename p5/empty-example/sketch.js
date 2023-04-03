ebcefb59717dad35757e8285cff04f2320f49903
Vorple-Sky <hannahg3211@gmail.com>
2023-04-03 10:38:32 -0700

    changed to grid example

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,8 @@
-let origx = 200;
-let origy = 100;
-let destx = 200;
-let desty = 300;
+let xpos = 80;
+let xstep = 30;
+let ypos = 140;
+let ystep = 30;
+
 
 function setup() {
   // put setup code here
@@ -12,17 +13,14 @@ function setup() {
 function draw() {
   // put drawing code here
   background(255);
-  fill(255);
-  strokeWeight(3);
+  fill(50);
+  noStroke();
 
-  for (let i = 25; i <400; i+=25){
-    stroke(50);
-    line(i, 200, origx, origy);
-    stroke(240);
-    line(i, 200, destx, desty);
+  for(let j =0; j <5; j++){
+    for (let i = 0; i < 9; i++){
+      ellipse(xpos + (xstep*i), ypos + (ystep*j), 20, 20)
 
+    }
   }
-
-
 
   }
